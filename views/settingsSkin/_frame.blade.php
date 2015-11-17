b4a8c3a99c3d6bb501529d77ba4fc43d45ee5fdf
akasima <akasima@nate.com>
2015-11-17 09:48:35 +0900

    #1 관리자 제목영역 설정

--- views/settingsSkin/_frame.blade.php
+++ views/settingsSkin/_frame.blade.php
@@ -1,3 +1,11 @@
+@section('page_title')
+    <h2>신고관리</h2>
+@stop
+
+@section('page_description')
+    신고를 관리하는 페이지 입니다.
+@stop
+
 {{ Frontend::js('plugins/xe_claim/DefaultManagerSkin/assets/board.js')->load() }}
 {{-- include contents blade file --}}
 @yield('content', isset($content) ? $content : '')
--- views/settingsSkin/index.blade.php
+++ views/settingsSkin/index.blade.php
@@ -1,10 +1,3 @@
-@section('page_title')
-    <h2>신고관리</h2>
-@stop
-
-@section('page_description')
-    신고를 관리하는 페이지 입니다.
-@stop
 <div class="panel">
     <div class="panel-heading">
 
