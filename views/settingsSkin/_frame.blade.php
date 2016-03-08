b1873d23a3242dabb9d6002f44e5b6837f922af0
khongchi <sungbum00@gmail.com>
2016-03-08 13:35:13 +0900

    change Frontend -> XeFrontend

--- views/settingsSkin/_frame.blade.php
+++ views/settingsSkin/_frame.blade.php
@@ -6,6 +6,6 @@
     신고를 관리하는 페이지 입니다.
 @stop
 
-{{ Frontend::js('plugins/xe_claim/DefaultManagerSkin/assets/board.js')->load() }}
+{{ XeFrontend::js('plugins/xe_claim/DefaultManagerSkin/assets/board.js')->load() }}
 {{-- include contents blade file --}}
 @yield('content', isset($content) ? $content : '')
