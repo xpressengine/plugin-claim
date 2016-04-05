af2df7c4a129485ab7199208963fc6070065ac27
bnu <bnufactory@gmail.com>
2016-04-05 17:40:49 +0900

    alertBox() 대체

--- views/settingsSkin/index.blade.php
+++ views/settingsSkin/index.blade.php
@@ -78,7 +78,7 @@
                     {{--var responseText = $.parseJSON(response.responseText);--}}
                     {{--var type = 'danger';--}}
                     {{--var errorMessage = responseText.message;--}}
-                    {{--alertBox(type, errorMessage);--}}
+                    {{--XE.toast(type, errorMessage);--}}
                     {{--self.openStep('close');--}}
                 {{--}--}}
             {{--});--}}
