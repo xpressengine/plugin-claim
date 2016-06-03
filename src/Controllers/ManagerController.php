1e6817bd8f41acc74f7aaa850199a646ad0082f0
bnu <bnufactory@gmail.com>
2016-06-03 17:05:22 +0900

    라이선스 및 카피라이트 표기 정리

--- src/Controllers/ManagerController.php
+++ src/Controllers/ManagerController.php
@@ -2,15 +2,15 @@
 /**
  * Claim manager controller
  *
- * PHP version 5
- *
  * @category    Claim
  * @package     Claim
- * @author      XE Team (akasima) <osh@xpressengine.com>
- * @copyright   2014 Copyright (C) NAVER <http://www.navercorp.com>
- * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
- * @link        http://www.xpressengine.com
+ * @author      XE Developers <developers@xpressengine.com>
+ * @copyright   2015 Copyright (C) NAVER Corp. <http://www.navercorp.com>
+ * @license     LGPL-2.1
+ * @license     http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * @link        https://xpressengine.io
  */
+
 namespace Xpressengine\Plugins\Claim\Controllers;
 
 use Input;
@@ -30,10 +30,6 @@ use Xpressengine\Plugins\Claim\Models\ClaimLog;
  *
  * @category    Claim
  * @package     Claim
- * @author      XE Team (akasima) <osh@xpressengine.com>
- * @copyright   2014 Copyright (C) NAVER <http://www.navercorp.com>
- * @license     http://www.gnu.org/licenses/lgpl-3.0-standalone.html LGPL
- * @link        http://www.xpressengine.com
  */
 class ManagerController extends Controller
 {
